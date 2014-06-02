e6cf04bb705feac60a59e9d85e127ab9acf33bec
ruebenramirez <ruebenramirez@gmail.com>
2014-06-02 01:42:34 -0500

    working test for the python any() function

---
+++ any_testing.py
@@ -0,0 +1,34 @@
+
+class Foo(object):
+    def __init__(self, bar):
+        self.bar = someVar
+
+import testtools
+class TestPythonAnyFunction(testtools.TestCase):
+
+    def setUp(self):
+        super(TestPythonAnyFunction, self).setUp()
+        self.tri1 = Triangle(1)
+        self.tri2 = Triangle(2)
+        self.tri3 = Triangle(3)
+        self.tri4 = Triangle(4)
+
+        self.my_tris = [self.tri1, self.tri2, self.tri3]
+
+    def tearDown(self):
+        super(TestPythonAnyFunction, self).tearDown()
+
+    def test_find_correct_objects_with_the_any_python_method(self):
+        tri1_found = any(self.tri1 == t for t in self.my_tris)
+        self.assertTrue(tri1_found)
+
+        tri2_found = any(self.tri2 == t for t in self.my_tris)
+        self.assertTrue(tri2_found)
+
+        tri3_found = any(self.tri3 == t for t in self.my_tris)
+        self.assertTrue(tri3_found)
+
+        # tri4 is not in the list and should be false on lookup
+        tri4_found = any(self.tri4 == t for t in self.my_tris)
+        self.assertFalse(tri4_found)
+
